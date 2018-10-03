bf2e7512f922a4f3306891597be2839b9b0b4188
Infiniumz <artuggg1@gmail.com>
2018-10-03 16:59:31 +0300

    encode/decode solved

--- lecture03/src/main/kotlin/io/rybalkinsd/kotlinbootcamp/practice/encoding.kt
+++ lecture03/src/main/kotlin/io/rybalkinsd/kotlinbootcamp/practice/encoding.kt
@@ -1,6 +1,5 @@
 package io.rybalkinsd.kotlinbootcamp.practice
 
-
 /**
  * NATO phonetic alphabet
  */
@@ -10,7 +9,7 @@ val alphabet = setOf("Alfa", "Bravo", "Charlie", "Delta", "Echo", "Foxtrot", "Go
  * A mapping for english characters to phonetic alphabet.
  * [ a -> Alfa, b -> Bravo, ...]
  */
-val association: Map<Char, String> = TODO()
+val association: Map<Char, String> = alphabet.associate { it[0].toLowerCase() to it }
 
 /**
  * Extension function for String which encode it according to `association` mapping
@@ -21,14 +20,14 @@ val association: Map<Char, String> = TODO()
  * "abc".encode() == "AlfaBravoCharlie"
  *
  */
-fun String.encode(): String = TODO()
+fun String.encode(): String = map { it.toLowerCase() }.map { association.get(it) ?: it }.joinToString("")
 
 /**
  * A reversed mapping for association
  * [ alpha -> a, bravo -> b, ...]
  */
-val reversedAssociation: Map<String, Char> = TODO()
 
+val reversedAssociation: Map<String, Char> = alphabet.associate { it to it[0].toLowerCase() }
 
 /**
  * Extension function for String which decode it according to `reversedAssociation` mapping
@@ -36,11 +35,33 @@ val reversedAssociation: Map<String, Char> = TODO()
  * @return encoded string or null if it is impossible to decode
  *
  * Example:
- * "alphabravocharlie".encode() == "abc"
+ * "alphabravocharlie".decode() == "abc"
  * "charliee".decode() == null
  *
  */
-fun String.decode(): String? = TODO()
+
+fun String.decode(): String? {
+    var str: String = ""
+    var i: Int = 0
+    while (i < this.length) {
+        val c: Char = this[i].toLowerCase()
+        if (association.containsKey(this[i].toLowerCase())) {
+            val str1: String ? = association.get(this[i].toLowerCase())
+            str1!!.forEach {
+                if (it == this[i]) {
+                    ++i
+                } else {
+                    return null
+                }
+            }
+            str += c
+        } else {
+            str += this[i]
+            ++i
+        }
+    }
+    return str
+}
 
 
 
