77ae4dc8c7688980407265b5ae3265e3a4ee1252
Infiniumz <artuggg1@gmail.com>
2018-10-12 03:08:29 +0300

    assignment03 is done!

--- assignments/03/src/main/kotlin/io/rybalkinsd/kotlinbootcamp/DataAnalysis.kt
+++ assignments/03/src/main/kotlin/io/rybalkinsd/kotlinbootcamp/DataAnalysis.kt
@@ -1,6 +1,68 @@
 package io.rybalkinsd.kotlinbootcamp
 
-class RawProfile(val rawData: String)
+/**
+ * Here are Raw profiles to analyse
+ */
+
+val rawProfiles = listOf(
+        RawProfile("""
+            firstName=alice,
+            age=16,
+            source=facebook
+            """.trimIndent()
+        ),
+        RawProfile("""
+            firstName=dent,
+            lastName=kent,
+            age=88,
+            source=linkedin
+            """.trimIndent()
+        ),
+        RawProfile("""
+            firstName=alla,
+            lastName=OloOlooLoasla,
+            source=vk
+            """.trimIndent()
+        ),
+        RawProfile("""
+            firstName=bella,
+            age=36,
+            source=vk
+            """.trimIndent()
+        ),
+        RawProfile("""
+            firstName=angel,
+            age=I will not tell you =),
+            source=facebook
+            """.trimIndent()
+        ),
+
+        RawProfile("""
+            lastName=carol,
+            source=vk,
+            age=49
+            """.trimIndent()
+        ),
+        RawProfile("""
+            firstName=bob,
+            lastName=John,
+            age=47,
+            source=linkedin
+            """.trimIndent()
+        ),
+        RawProfile("""
+            lastName=kent,
+            firstName=dent,
+            age=88,
+            source=facebook
+            """.trimIndent()
+        )
+)
+class RawProfile(val rawData: String) {
+    override fun toString() : String {
+       return this.rawData
+    }
+}
 
 enum class DataSource {
     FACEBOOK,
@@ -20,80 +82,141 @@ sealed class Profile(
  * Task #1
  * Declare classes for all data sources
  */
-class FacebookProfile(id: Long) : Profile(dataSource = DataSource.FACEBOOK, id = id)
+class FacebookProfile(id: Long) : Profile(dataSource = DataSource.FACEBOOK, id = id) {
+    override fun equals(other: Any?): Boolean {
+        return when (other) {
+            is Profile -> firstName == other.firstName && lastName == other.lastName && age == other.age
+            else -> false
+        }
+    }
+}
 
+class LinkedInProfile(id: Long) : Profile(dataSource = DataSource.FACEBOOK, id = id)
+
+class VkProfile(id: Long) : Profile(dataSource = DataSource.FACEBOOK, id = id)
+
+fun ParseToSet(e: List<RawProfile>) : Set<Profile> {
+    var st = mutableSetOf<Profile>()
+    var id = mutableListOf(0L, 0L, 0L)
+    var firstName : String? = null
+    var lastName : String? = null
+    var age : Int? = null
+    var dataSourcet : String = ""
+    e.forEach {
+        val str = it.toString().replace("\n", "").split(",")
+        str.forEach {
+            val lpart = it.substringBefore('=')
+            val rpart = it.substringAfter('=')
+            when (lpart) {
+                "firstName" -> firstName = rpart
+                "lastName" -> lastName = rpart
+                "age" -> age = rpart.toIntOrNull()
+                "source" -> dataSourcet = rpart
+            }
+            }
+        when(dataSourcet) {
+            "facebook" -> {
+                st.add(FacebookProfile(id[0]).apply {
+                    this.dataSource = DataSource.FACEBOOK
+                    this.firstName = firstName
+                    this.lastName = lastName
+                    this.age = age
+                })
+                id[0]++
+            }
+            "linkedin" -> {
+                st.add(LinkedInProfile(id[1]).apply {
+                    this.dataSource = DataSource.LINKEDIN
+                    this.firstName = firstName
+                    this.lastName = lastName
+                    this.age = age
+                })
+                id[1]++
+            }
+            else -> {
+                st.add(VkProfile(id[2]).apply {
+                    this.dataSource = DataSource.VK
+                    this.firstName = firstName
+                    this.lastName = lastName
+                    this.age = age
+                })
+                id[2]++
+            }
+        }.also {
+            firstName = null
+            lastName = null
+            age  = null
+            dataSourcet = ""
+        }
+    }
+    return st.toSet()
+}
+
+var ProfileSet: Set<Profile> = ParseToSet(rawProfiles)
 /**
  * Task #2
  * Find the average age for each datasource (for profiles that has age)
  *
  * TODO
  */
-val avgAge: Map<DataSource, Double> = emptyMap()
 
+fun avg(e: Set<Profile>) : Map<DataSource, Double> {
+    var mp = mutableMapOf(Pair(DataSource.FACEBOOK, 0.0),
+            Pair(DataSource.LINKEDIN, 0.0), Pair(DataSource.VK, 0.0))
+    var lst = mutableListOf(0, 0, 0)
+    e.forEach {
+        when (it.dataSource) {
+            DataSource.FACEBOOK -> {
+                if (it.age != null) {
+                    mp[DataSource.FACEBOOK] = it.age!!.toDouble() + mp[DataSource.FACEBOOK]!!
+                    lst[0]++
+                }
+            }
+            DataSource.LINKEDIN -> {
+                if (it.age != null) {
+                    mp[DataSource.LINKEDIN] = it.age!!.toDouble() + mp[DataSource.LINKEDIN]!!
+                    lst[1]++
+                }
+            }
+            DataSource.VK -> {
+                if (it.age != null) {
+                    mp[DataSource.VK] = it.age!!.toDouble() + mp[DataSource.VK]!!
+                    lst[2]++
+                }
+            }
+        }
+    }
+    mp[DataSource.FACEBOOK] = mp[DataSource.FACEBOOK]!! / lst[0].toDouble()
+    mp[DataSource.LINKEDIN] = mp[DataSource.LINKEDIN]!! / lst[1].toDouble()
+    mp[DataSource.VK] = mp[DataSource.VK]!! / lst[2].toDouble()
+    return mp.toMap()
+}
+
+var avgAge = avg(ProfileSet)
 /**
- * Task #3
  * Group all user ids together with all profiles of this user.
  * We can assume users equality by : firstName & lastName & age
  *
  * TODO
  */
-val groupedProfiles: Map<Long, List<Profile>> = emptyMap()
 
-/**
- * Here are Raw profiles to analyse
- */
-val rawProfiles = listOf(
-    RawProfile("""
-            fisrtName=alice,
-            age=16,
-            source=facebook
-            """.trimIndent()
-    ),
-    RawProfile("""
-            fisrtName=Dent,
-            lastName=kent,
-            age=88,
-            source=linkedin
-            """.trimIndent()
-    ),
-    RawProfile("""
-            fisrtName=alla,
-            lastName=OloOlooLoasla,
-            source=vk
-            """.trimIndent()
-    ),
-    RawProfile("""
-            fisrtName=bella,
-            age=36,
-            source=vk
-            """.trimIndent()
-    ),
-    RawProfile("""
-            fisrtName=angel,
-            age=I will not tell you =),
-            source=facebook
-            """.trimIndent()
-    ),
+fun toGroup(e: Set<Profile>) : Map<Long, List<Profile>> {
+    var mp = mutableMapOf<Long, List<Profile>>()
+    var ind = 0L
+    val tmp2 = e.toMutableList()
+    while(tmp2.isNotEmpty()) {
+        val tmp = tmp2[0]
+        val ans = tmp2.filter{ it == tmp }
+        ans.forEach {
+            mp.put(ind, ans)
+        }
+        ans.forEach {
+            tmp2.remove(it)
+        }
+        ind++
+    }
+    return mp.toMap()
+}
 
-    RawProfile(
-        """
-            lastName=carol,
-            source=vk
-            age=49,
-            """.trimIndent()
-    ),
-    RawProfile("""
-            fisrtName=bob,
-            lastName=John,
-            age=47,
-            source=linkedin
-            """.trimIndent()
-    ),
-    RawProfile("""
-            lastName=kent,
-            fisrtName=dent,
-            age=88,
-            source=facebook
-            """.trimIndent()
-    )
-)
+val groupedProfiles: Map<Long, List<Profile>> = toGroup(ProfileSet)
+
--- assignments/03/src/test/kotlin/io/rybalkinsd/kotlinbootcamp/DataAnalysisTest.kt
+++ assignments/03/src/test/kotlin/io/rybalkinsd/kotlinbootcamp/DataAnalysisTest.kt
@@ -1,8 +1,8 @@
 package io.rybalkinsd.kotlinbootcamp
 
+import junit.framework.TestCase.assertEquals
 import junit.framework.TestCase.assertTrue
 import org.junit.Test
-
 
 class DataAnalysisTest {
 
@@ -14,5 +14,54 @@ class DataAnalysisTest {
     @Test
     fun `check grouped profiles`() {
         assertTrue(groupedProfiles.isNotEmpty())
+    }
+
+    @Test
+    fun `check avg age2`() {
+        val tmptest = listOf(
+                RawProfile("""
+            firstName=alice,
+            age=22,
+            source=facebook
+            """.trimIndent()
+                ),
+                RawProfile("""
+            firstName=dent,
+            lastName=kent,
+            age=40,
+            source=facebook
+            """.trimIndent()
+                ),
+                RawProfile("""
+            firstName=bella,
+            age=30,
+            source=vk
+            """.trimIndent()
+                ),
+
+                RawProfile("""
+            lastName=carol,
+            source=vk,
+            age=50
+            """.trimIndent()
+                ),
+                RawProfile("""
+            firstName=bob,
+            lastName=John,
+            age=50,
+            source=linkedin
+            """.trimIndent()
+                ),
+                RawProfile("""
+            lastName=kent,
+            firstName=dent,
+            age=10,
+            source=linkedin
+            """.trimIndent()
+                ))
+        val p = avg(ParseToSet(tmptest))
+        assertEquals(40.0, p[DataSource.VK])
+        assertEquals(30.0, p[DataSource.LINKEDIN])
+        assertEquals(31.0, p[DataSource.FACEBOOK])
     }
 }
